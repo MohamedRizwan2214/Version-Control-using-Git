edd9cabbf4df89799b398bfd40a49fb4e1559eb9
MohamedRizwan2214 <MohamedRizwan2214@gmail.com>
2025-03-19 14:33:50 +0530

    Task 7

---
+++ Task 7 Cherry-Picking Commits Between Branches/index.js
@@ -0,0 +1,4 @@
+//Objective -> 1)make changes in branch2 
+            // 2)cherry-pick the changes in branch1
+            // 3)make changes in main
+console.log("Task 7 Cherry-Picking Commits Between Branches");
