adf47f7b49974924981749e6c7bd14108f0fa9f3
MohamedRizwan2214 <MohamedRizwan2214@gmail.com>
2025-03-19 14:40:40 +0530

    Bug fix from branch 1

--- Task 7 Cherry-Picking Commits Between Branches/index.js
+++ Task 7 Cherry-Picking Commits Between Branches/index.js
@@ -2,3 +2,5 @@
             // 2)cherry-pick the changes in branch1
             // 3)make changes in main
 console.log("Task 7 Cherry-Picking Commits Between Branches");
+
+console.log("bug Fix of feature ")//done in branch_1(Bug Fix Branch)
