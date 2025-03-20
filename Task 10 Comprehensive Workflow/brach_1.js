b3dd00cba70856c1a577a0165414b71acf43a8b5
MohamedRizwan2214 <MohamedRizwan2214@gmail.com>
2025-03-20 11:53:42 +0530

    fixing bugs

--- Task 10 Comprehensive Workflow/brach_1.js
+++ Task 10 Comprehensive Workflow/brach_1.js
@@ -1,1 +1,2 @@
 console.log("Working on a page ")//from branch_1
+console.log("fixing a bug")//from bug-fix branch
--- Task 10 Comprehensive Workflow/branch_2.js
+++ Task 10 Comprehensive Workflow/branch_2.js
@@ -1,1 +1,2 @@
 console.log("Working on a page "); //from branch_2
+console.log("fixing a bug")//from bug-fix branch
